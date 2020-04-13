ce1b141a7f48cbf1df10ff097156a281f471e602
gkonto <geokonto92@gmail.com>
2020-04-13 14:37:29 +0300

    Added auxiliaries.hpp file

---
+++ auxiliaries.hpp
@@ -0,0 +1,12 @@
+#ifndef AUXILIARIES_HPP
+#define AUXILIARIES_HPP
+
+#define BLUE    "\033[1m\033[34m"
+#define CYAN    "\033[1m\033[36m"
+#define RED     "\033[1m\033[31m"
+#define GREEN   "\033[1m\033[32m"
+#define BOLD    "\033[1m"
+#define RESET   "\033[0m"
+
+
+#endif
