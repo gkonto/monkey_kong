902d89d2286f3b63b9b94cea23bb95c4444a23bf
gkonto <geokonto92@gmail.com>
2020-04-13 14:13:29 +0300

    Added QA main function

--- qa/main.cpp
+++ qa/main.cpp
@@ -1,5 +1,14 @@
+#include "../argparser.hpp"
+
+struct QAUserArgs
+{
+};
 
 int main(int argc, char **argv)
 {
+    QAUserArgs u_args;
+    ArgParser a_parser("Interpreter QA mechanism", argc, argv);
+    a_parser.addArgument("--help", "Display this information", nullptr);
 
+    a_parser.parse(nullptr);
 }
