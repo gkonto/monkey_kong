997a12249630a6bffa6beb48bc2c45774e20c8c0
gkonto <geokonto92@gmail.com>
2020-04-14 08:39:12 +0300

    Added builtin symbols

---
+++ symbols.cpp
@@ -0,0 +1,40 @@
+#include "symbols.hpp"
+#include "token.hpp"
+#include <iostream>
+
+
+
+TokenType SymbolTable::LookupIdent(const std::string &key)
+{
+    SymbolTable &instance = getInstance();
+    auto val = instance.keywords.find(key);
+
+    if (val != instance.keywords.end()) {
+        return val->second;
+    }
+
+    return T_IDENT;
+}
+
+void SymbolTable::initializeSymbols()
+{
+    keywords.emplace("fn", T_FUNCTION);
+    keywords.emplace("let", T_LET);
+    keywords.emplace("true", T_TRUE);
+    keywords.emplace("false", T_FALSE);
+    keywords.emplace("if", T_IF);
+    keywords.emplace("else", T_ELSE);
+    keywords.emplace("return", T_RETURN);
+
+}
+
+SymbolTable::SymbolTable()
+{
+    initializeSymbols();
+}
+
+SymbolTable &SymbolTable::getInstance()
+{
+    static SymbolTable s_table;
+    return s_table;
+}
---
+++ symbols.hpp
@@ -0,0 +1,22 @@
+#ifndef SYMBOLS_HPP
+#define SYMBOLS_HPP
+
+#include <map>
+#include <string>
+
+#include "token.hpp"
+
+class SymbolTable
+{
+    public:
+        static SymbolTable &getInstance();
+        static TokenType LookupIdent(const std::string &key);
+
+    private:
+        SymbolTable();
+        void initializeSymbols();
+        std::map<std::string, TokenType> keywords;
+};
+
+
+#endif
